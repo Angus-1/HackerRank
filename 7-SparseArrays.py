2965c0bd74811ef67595a5f5e8fa0685dc803d26
Angel <santoyox714@gmail.com>
2022-06-08 11:38:06 -0700

    Create 7-SparseArrays.py

---
+++ 7-SparseArrays.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def matchingStrings(strings, queries):
+    return [strings.count(i) for i in queries]
+    #.count method returns number each time element occurs in queries list 
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
