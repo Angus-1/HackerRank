e8c78f4f2cc74ec40798b4e6c78bb4a7b951faa7
Angel <santoyox714@gmail.com>
2022-06-08 11:40:53 -0700

    Update 6-DivisibleSumPairs.py

--- 6-DivisibleSumPairs.py
+++ 6-DivisibleSumPairs.py
@@ -7,7 +7,9 @@ import re
 import sys
 import itertools
 
-
+#  Given an array of integers and a positive integer k, determine 
+#  the number of (i,j)  pairs where  (i<j) and ar[i] + ar[j]  is
+#  divisible by k.
         
 def divisibleSumPairs(n, k, ar):
   pairs=0
