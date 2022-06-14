82634796f94bd7ce4e10983c5f21737a74a80e7a
Angel <santoyox714@gmail.com>
2022-06-14 10:29:14 -0700

    Update w3-18-SalesByMatch.py

--- w3-18-SalesByMatch.py
+++ w3-18-SalesByMatch.py
@@ -6,12 +6,12 @@ import random
 import re
 import sys
 
-# 1 1 1 2 2 2 3 3 
-#current pairs 3(1, 2, 3)
+#current array   ( 1 1 1 2 2 2 3 3 )
+#current colors  (1, 2, 3)
 def sockMerchant(n, ar):
     colors:set = set(ar) #make set of all possible colors
     pairs:int = 0        #num of curr pairs 0
-    for c in colors:     #iterate through colors
+    for c in colors:     #use colors to iterate through ar 
         pairs += ar.count(c) // 2   #count returns number of each colors total occurences
                                      #and we divide by 2 with floor operator giving us pairs without extras 
         
