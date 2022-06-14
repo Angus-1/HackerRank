24cff53b308fc1ca5e8c1a58b36b59d972238d20
Angel <santoyox714@gmail.com>
2022-06-14 12:16:09 -0700

    Create w3-19-MaximumPerimeterTriangle.py

---
+++ w3-19-MaximumPerimeterTriangle.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#1,2,3,4,5,10
+#two subarrays below form trianlges
+#2,3,4 
+#3,4,5
+#Choose the one with the longest maximum side 
+#if none exists return -1
+
+def maximumPerimeterTriangle(s):
+   s = sorted(sticks) 
+   b = [-1] 
+                  
+   for i in range(len(s)):
+    a = s[i : i+2]              #slice array in indexes of 3 and assign to a
+    if(s[i] + s[i+1] > s[i+2]): #check triangle condition
+        return a                #get subarray with those elements, 
+                                #since sorted, a will be returned once we have largest possible triangle
+    return (b)                  #no triangle exist condition
+ 
+
+def maximumPerimeterTriangle(sticks):
+    arr = sorted(sticks)
+    res = [-1]
+    for i in range(len(arr)-2):
+        cut = arr[i:i+3]
+        if cut[0] + cut[1] > cut[2]:
+            res = cut
+    return res
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    sticks = list(map(int, input().rstrip().split()))
+
+    result = maximumPerimeterTriangle(sticks)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
