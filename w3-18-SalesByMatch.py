1f4aaf01c05d6036b9f2ec854ecff945e51f73f1
Angel <santoyox714@gmail.com>
2022-06-14 10:27:38 -0700

    Create w3-18-SalesByMatch.py

---
+++ w3-18-SalesByMatch.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# 1 1 1 2 2 2 3 3 
+#current pairs 3(1, 2, 3)
+def sockMerchant(n, ar):
+    colors:set = set(ar) #make set of all possible colors
+    pairs:int = 0        #num of curr pairs 0
+    for c in colors:     #iterate through colors
+        pairs += ar.count(c) // 2   #count returns number of each colors total occurences
+                                     #and we divide by 2 with floor operator giving us pairs without extras 
+        
+    return pairs 
+     
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
